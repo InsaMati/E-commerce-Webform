8b2f2687bb648ab4cabb4944bab4607f803157c4
Matias <matiasinsaurralde51@gmail.com>
2020-10-29 03:55:26 -0300

    Ventana Agregar Producto

--- TPC_Orihuela_Insaurralde/ProductoA.aspx
+++ TPC_Orihuela_Insaurralde/ProductoA.aspx
@@ -1,6 +1,58 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ProductoA.aspx.cs" Inherits="TPC_Orihuela_Insaurralde.ProductoA" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="container-fluid"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <asp:Label Text="Codigo" Style="font-size: 15px; color: orangered" runat="server" /><br />
+                <br />
+                <asp:TextBox runat="server" Style="font-size: 15px" CssClass="form-control" ID="TxtCodigo" />
+            </div>
+            <div class="col-md-4">
+                <asp:Label Text="Nombre" Style="font-size: 15px; color: orangered" runat="server" /><br />
+                <br />
+                <asp:TextBox Style="font-size: 15px" CssClass="form-control" ID="TxtNombre" runat="server" />
+            </div>
+            <div class="col-md-4">
+                <asp:Label Text="Descripcion" Style="font-size: 15px; color: orangered" runat="server" /><br />
+                <br />
+                <asp:TextBox Style="font-size: 15px" CssClass="form-control" ID="TxtDescripcion" runat="server" />
+                <br />
+                <br />
+            </div>
 
+
+
+            <div class="col-md-4">
+                <asp:Label Text="Marca" Style="font-size: 15px; color: orangered" runat="server" /><br />
+                <br />
+                <asp:DropDownList runat="server" CssClass="form-control" Style="font-size: 15px" ID="DDMarca" />
+            </div>
+            <div class="col-md-4">
+                <asp:Label Text="Categoria" Style="font-size: 15px; color: orangered" runat="server" /><br />
+                <br />
+                <asp:DropDownList runat="server" CssClass="form-control" Style="font-size: 15px" ID="DDCategoria" />
+            </div>
+            <div class="col-md-4">
+                <asp:Label Text="Imagen" Style="font-size: 15px; color: orangered" runat="server" /><br />
+                <br />
+                <asp:TextBox Style="font-size: 15px" CssClass="form-control" ID="TxtImagen" runat="server" />
+                <br />
+                <br />
+            </div>
+
+            <div class="col-md-4">
+                <asp:Label Text="Precio" Style="font-size: 15px; color: orangered" runat="server" /><br />
+                <br />
+                <asp:TextBox runat="server" Style="font-size: 15px" CssClass="form-control" ID="TxtPrecio" />
+                <br />
+            </div>
+
+        </div>
+    </div>
+    <center>
+    <asp:Button Text="Agregar" ID="BtnAgregar" class="btn btn-primary" Style="font-size:15px;background-color:orangered" runat="server"/>
+        <asp:Button Text="Volver" ID="BtnVolver" class="btn btn-primary" Style="font-size:15px;background-color:orangered" runat="server"/>
+  </center>
 </asp:Content>
--- TPC_Orihuela_Insaurralde/ProductoA.aspx.designer.cs
+++ TPC_Orihuela_Insaurralde/ProductoA.aspx.designer.cs
@@ -13,5 +13,86 @@ namespace TPC_Orihuela_Insaurralde
 
     public partial class ProductoA
     {
+
+        /// <summary>
+        /// Control TxtCodigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtCodigo;
+
+        /// <summary>
+        /// Control TxtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtNombre;
+
+        /// <summary>
+        /// Control TxtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtDescripcion;
+
+        /// <summary>
+        /// Control DDMarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDMarca;
+
+        /// <summary>
+        /// Control DDCategoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDCategoria;
+
+        /// <summary>
+        /// Control TxtImagen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtImagen;
+
+        /// <summary>
+        /// Control TxtPrecio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtPrecio;
+
+        /// <summary>
+        /// Control BtnAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnAgregar;
+
+        /// <summary>
+        /// Control BtnVolver.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnVolver;
     }
 }
