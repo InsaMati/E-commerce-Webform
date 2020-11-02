4f845d2423c4d642eb50deb0352268df479207b6
Matias <matiasinsaurralde51@gmail.com>
2020-11-02 12:28:33 -0300

    seccion ABML Categoria agregada (sin funcionalidad) solo listar

--- TPC_Orihuela_Insaurralde/ABMLCategoria.aspx
+++ TPC_Orihuela_Insaurralde/ABMLCategoria.aspx
@@ -1,5 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ABMLMarca.aspx.cs" Inherits="TPC_Orihuela_Insaurralde.ABMLMarca" %>
-
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ABMLCategoria.aspx.cs" Inherits="TPC_Orihuela_Insaurralde.ABMLCategoria" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
     <div class="container">
---
+++ TPC_Orihuela_Insaurralde/ABMLCategoria.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPC_Orihuela_Insaurralde
+{
+    public partial class ABMLCategoria : System.Web.UI.Page
+    {
+        public List<Categoria> Lista = new List<Categoria>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                NegocioCategoria Negocio = new NegocioCategoria();
+                Lista = Negocio.ListarCategorias();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
