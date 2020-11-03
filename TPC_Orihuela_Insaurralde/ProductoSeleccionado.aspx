a76dd358f14906ebb0e01d5dcdd8faa5280d86a4
Lucas <lucasorihuela18@gmail.com>
2020-11-02 21:12:45 -0300

    Pagina de detalle de cada producto

--- TPC_Orihuela_Insaurralde/ProductoSeleccionado.aspx
+++ TPC_Orihuela_Insaurralde/ProductoSeleccionado.aspx
@@ -1,3 +1,11 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ProductoSeleccionado.aspx.cs" Inherits="TPC_Orihuela_Insaurralde.ProductoSeleccionado" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <br />
+    <img src="<% = MostrarArticulo.UrlImagen %>" width="250"  height="300" alt="No se encontro la imagen" />
+    <h5> <% = MostrarArticulo.Nombre %> </h5>
+    <p> <% = MostrarArticulo.Descripcion %> </p>
+    <p> Precio: <% = MostrarArticulo.Precio %> </p>
+
+
 </asp:Content>
--- TPC_Orihuela_Insaurralde/ProductoSeleccionado.aspx.cs
+++ TPC_Orihuela_Insaurralde/ProductoSeleccionado.aspx.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace TPC_Orihuela_Insaurralde
 {
     public partial class ProductoSeleccionado : System.Web.UI.Page
     {
+        public Articulo MostrarArticulo { get; set; }
+        public List<Articulo> ListaArticulos { get; set; }
+
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            NegocioArticulo NegArticulo = new NegocioArticulo();
+
+            try
+            {
+                ListaArticulos = NegArticulo.ListarArticulos();
+                var ArtSelec = Convert.ToInt32(Request.QueryString["Id"]);
+                MostrarArticulo = ListaArticulos.Find(J => J.Id == ArtSelec);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
 
         }
     }
